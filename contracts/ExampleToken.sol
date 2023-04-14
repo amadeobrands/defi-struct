845b9c9f875578077a647488386da5b0e50d8126
Bogdanoff <pumpit@mailinator.com>
2023-04-14 20:54:29 +0700

    add initial vault and a test token

---
+++ contracts/ExampleToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
+contract Vault is ERC4626 {
+    constructor(IERC20Metadata asset) ERC20("vExampleToken", "vEEE") ERC4626(asset) {}
+}
+
+contract ExampleToken is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("ExampleToken", "EEE") {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
+
